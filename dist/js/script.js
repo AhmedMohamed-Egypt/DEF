94ead81dbf4f07c0deaf7beb1e9c28cfaf36a968
Ahmed-Mohamed <you@example.com>
2024-02-19 17:19:27 +0200

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,31 @@
 //get src for hero image to use instead of html for CMS integration
-console.log(55)
+const swiper = new Swiper('.getTickes  .swiper', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 5,
+    speed:500,
+   
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+       
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        
+      }
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  })
+  
