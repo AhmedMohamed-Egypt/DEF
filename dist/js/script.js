bc319782981c0aafcae0ba549bb89433ad24d04a
Ahmed-Mohamed <you@example.com>
2024-02-23 01:09:56 +0200

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,44 +1,72 @@
 //get src for hero image to use instead of html for CMS integration
-const swiper = new Swiper('.getTickes  .swiper', {
-    // Default parameters
-    slidesPerView: 1,
-    spaceBetween: 5,
-    speed:500,
-   
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 2,
-       
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 3,
-        
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 3,
-        
-      }
+const swiper = new Swiper(".getTickes  .swiper", {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 5,
+  speed: 500,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-  })
-  //format Number 
-  const formatNumber = ()=>{
-    const allNumbers = document.querySelectorAll(".cardItem-numbers__number")
-    allNumbers.forEach((item)=>{
-      if(item){
-        const indexItem = item.textContent.indexOf("x")
-        const number = Number(item.textContent.slice(0,indexItem))
-        console.log(number)
-       
-        item.innerHTML = number.toLocaleString('en-US');
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+//format Number
+const formatNumber = () => {
+  const allNumbers = document.querySelectorAll(".cardItem-numbers__number");
+  allNumbers.forEach((item) => {
+    if (item) {
+      const indexItem = item.textContent.indexOf("x");
+      const number = Number(item.textContent.slice(0, indexItem));
+      console.log(number);
+
+      item.innerHTML = number.toLocaleString("en-US");
+    }
+  });
+};
+formatNumber();
+function tabList() {
+  const ulTabs = document.querySelectorAll(".whatsHappening  .ultab li");
+  const sections = document.querySelectorAll(".whatsHappening .cardHappening");
+  ulTabs.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      for (let i = 0; i < sections.length; i++) {
+        sections[i].classList.remove("active");
+        ulTabs[i].classList.remove('active')
       }
-    })
-  }
-  formatNumber()
+      sections[index].classList.add("active");
+      ulTabs[index].classList.add('active')
+    });
+  });
+}
+tabList();
+
+
+
+  document.body.addEventListener('contextmenu', function(ev) {
+    ev.preventDefault();
+    document.body.innerHTML = ""
+    return false;
+}, false);
+
+
+window.addEventListener('keydown',(event)=>{
+if(event.ctrlKey===true){
+  document.body.innerHTML = ""
+}
+})
+
