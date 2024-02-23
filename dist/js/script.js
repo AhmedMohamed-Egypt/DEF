38b6c39c9642d3e11f9e1e2ba407c059a6eead62
Ahmed-Mohamed <you@example.com>
2024-02-23 03:06:20 +0200

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -39,24 +39,63 @@ const formatNumber = () => {
   });
 };
 formatNumber();
+animateDisplay = function(target, animationClass, displayType, timeout) {
+  // timeout should be longer than css transition
+  var doneTimedDisplay = false,
+    displaying = false;
+
+  target.addEventListener('transitionend', function() {
+    if (!target.classList.contains('active')) {
+      target.style.display = 'none';
+    }
+    doneTimedDisplay = true;
+  });
+  if (!target.style.display || target.style.display === 'none') {
+    displaying = true;
+    target.style.display = displayType;
+  } else {
+    displaying = false;
+  }
+
+  window.requestAnimationFrame(function() {
+    target.classList.toggle(animationClass);
+    doneTimedDisplay = false;     
+  });
+
+  if (!displaying) {
+    setTimeout(function() {
+    
+      if (!doneTimedDisplay) {
+        target.style.display = 'none';
+      }
+      doneTimedDisplay = true;
+    }, timeout);
+  }
+};
 function tabList() {
   const ulTabs = document.querySelectorAll(".whatsHappening  .ultab li");
   const sections = document.querySelectorAll(".whatsHappening .cardHappening");
   ulTabs.forEach((item, index) => {
     item.addEventListener("click", () => {
+      
       for (let i = 0; i < sections.length; i++) {
-        sections[i].classList.remove("active");
+       // sections[i].classList.remove("active");
         ulTabs[i].classList.remove('active')
+       // animateDisplay(sections[i], 'active', 'none', 600)
+       sections[i].style.display="none"
+       sections[i].classList.remove('active')
       }
-      sections[index].classList.add("active");
+    //  sections[index].classList.add("active");
       ulTabs[index].classList.add('active')
+      animateDisplay(sections[index], 'active', 'flex', 1000)
+    
     });
   });
 }
 tabList();
 
-
-
+//
+/*
   document.body.addEventListener('contextmenu', function(ev) {
     ev.preventDefault();
     document.body.innerHTML = ""
@@ -70,3 +109,4 @@ if(event.ctrlKey===true){
 }
 })
 
+*/
